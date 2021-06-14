946968439ad61a5b43a58b5b46ce7240133bec02
Miqueias Lima <miqueiaslima@outlook.com>
2021-06-14 10:22:38 -0300

    Layout Tela Principal

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,60 @@
-import 'package:appatmconsultoria/AtmConsultoria.dart';
+import 'package:appatmconsultoria/TelaSecundaria.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 void main(){
   runApp(MaterialApp(
     title: "Flutter Aplication",
     debugShowCheckedModeBanner: false,
-    home: AtmConsultoria(),
+    home: TelaPrincipal(),
   ));
+}
+
+class TelaPrincipal extends StatefulWidget {
+  const TelaPrincipal({Key? key}) : super(key: key);
+
+  @override
+  _TelaPrincipalState createState() => _TelaPrincipalState();
+}
+
+class _TelaPrincipalState extends State<TelaPrincipal> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("ATM Consultorias"),
+        backgroundColor: Colors.blue,
+      ),
+      body: Container(
+        padding: EdgeInsets.only(top: 50,bottom: 50,right: 30,left: 30),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.all(20),
+              child: Image.asset("imagens/logo.png"),
+            ),
+            Row(
+              children: <Widget>[
+
+              ],
+            ),
+
+            ElevatedButton(onPressed: (){
+              Navigator.push(context, MaterialPageRoute(
+                  builder: (context) => TelaSecundaria()
+                )
+              );
+            }, child: Text("Tela Secundária"),
+            style: ElevatedButton.styleFrom(
+              onPrimary: Colors.white,
+              primary: Colors.blue,
+              padding: EdgeInsets.all(16)
+            ),)
+          ],
+        ),
+      ),
+    );
+  }
 }
